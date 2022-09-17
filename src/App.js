10b5a817d0554bba128c7e3af701a4e03047ed86
Elyes.Benkhoud <elyesbenkhoud377@gmail.com>
2022-09-17 22:12:19 +0300

    setup

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+import Header from "./component/Header";
+function App() {
+  return (
+    <>
+      <Header />
+    </>
+  );
+}
+
+export default App;
---
+++ src/component/Header.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <>
+      <header>
+        <ul class="left">
+          <li>Features</li>
+          <li>Company</li>
+          <li>Career</li>
+          <li>About</li>
+        </ul>
+        <ul class="right">
+          <li>Login</li>
+          <li>Register</li>
+        </ul>
+      </header>
+    </>
+  );
+};
+
+export default Header;
